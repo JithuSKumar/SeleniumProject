b5bfc66aeafe1bda6672a256f1212c21d3a3baab
USER <USER@192.168.1.47>
2024-08-19 08:08:57 +0530

    SuiteFiles_forCrossBroswer&ParallelExecution_Aug19

--- GroceryApplicationProject/src/test/java/stepFiles/BaseClassStep.java
+++ GroceryApplicationProject/src/test/java/stepFiles/BaseClassStep.java
@@ -1,9 +1,12 @@
 package stepFiles;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 
 public class BaseClassStep {
@@ -11,9 +14,17 @@ public class BaseClassStep {
 	WebDriver driver;
 	
   @BeforeMethod
-  public void beforeMethod() {
-	  
-	  driver = new ChromeDriver();
+  @Parameters("Browser")
+  public void beforeMethod(String browserName)
+  {
+	  if (browserName.equals("chrome"))
+	  {
+		  driver = new ChromeDriver();
+	  }
+	  else if (browserName.equals("firefox"))
+	  {
+		  driver = new FirefoxDriver();
+	  }
 	  driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 	  driver.manage().window().maximize();
 	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
@@ -21,7 +32,8 @@ public class BaseClassStep {
   }
 
   @AfterMethod
-  public void afterMethod() {
+  public void afterMethod() 
+  {
 	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
 	  driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(100));
 	  driver.close();
